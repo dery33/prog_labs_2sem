d5329bd68ff3ea1084270872ccb4d6d559fa8224
dery <162676304+dery33@users.noreply.github.com>
2026-05-12 21:12:48 +0300

    13

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 1234;
+        System.out.println(n % 1000 / 100); 
+        System.out.println(n % 100 / 10); 
+    
+        String str1 = "Hello";
+        String str2 = "World";
+        System.out.println(str1 + ", " + str2);
+    }
+}
